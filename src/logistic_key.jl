f594c6564637d409af4a2dcf310c4c0821ec3a44
Saransh <saransh0701@gmail.com>
2022-02-15 18:32:10 +0530

    Resolve type errors + docs

--- src/logistic_key.jl
+++ src/logistic_key.jl
@@ -3,20 +3,25 @@
 
 Generates pseudo-random keys using the Logistic Map.
 
+The equation -
+```math
+    \\x_{n+1} = r * \\x_{n} * (1 - \\x_{n})
+```
+
 # Arguments
 - `x_init::Float64`: Initial value of x. x ϵ (0, 1).
 - `r::Float64`: A constant value. Values > 4 usually results in pseudo-random numbers.
 - `num_keys::Int64`: Number of keys to be generated.
 
 # Returns
-- `keys::Array{Any, 1}`: Generated pseudo-random keys.
+- `keys::Array{Int64, 1}`: Generated pseudo-random keys.
 """
 function logistic_key(x_init::Float64, r::Float64, num_keys::Int64)
-    keys = []
+    keys = Array{Int64, 1}()
     x = x_init
 
     for i = 1:num_keys
-        x = r * x * (1-x)
+        x = r * x * (1 - x)
         key = x * 10 ^ 16 % 256
         push!(keys, trunc(Int, key))
     end
--- src/substitution.jl
+++ src/substitution.jl
@@ -2,14 +2,44 @@ using Images
 using FileIO
 
 """
-keys = logistic_key(0.01, 3.97, 719 * 718)
-    
-substitution_encryption("D:\\Saransh\\Docs\\PyCon_Squared.jpg", keys)
-substitution_decryption("./encrypted.png", keys)
+    substitution_encryption(path_to_image, keys, path_for_result="./encrypted.png")
+
+Performs substitution encryption on a given image with the given keys.
+
+# Algorithm
+Iterates simulataneously over each pixel and key, and XORs the pixel value
+(all R, G, and B) with the given key. Hence, the order of the keys matter.
+
+# Arguments
+- `path_to_image::String`: The path to the image to be encrypted.
+- `keys::Array{Int64, 1}`: Keys for encryption.
+- `path_for_result::String`: The path for storing the encrypted image.
+
+# Example
+```julia-repl
+julia> keys = logistic_key(0.01, 3.97, 719 * 718)
+516242-element Array{Any,1}:
+   0
+  44
+   7
+  26
+  14
+ 224
+   ⋮
+ 115
+  65
+ 126
+ 152
+  74
+ 198
+julia> substitution_encryption("D:\\Saransh\\Docs\\PyCon_Squared.jpg", keys)
+ENCRYPTING
+ENCRYPTED
+```
 """
 function substitution_encryption(
     path_to_image::String,
-    keys::Array,
+    keys::Array{Int64, 1},
     path_for_result::String="./encrypted.png",
 )
     image = load(path_to_image)
@@ -43,9 +73,32 @@ function substitution_encryption(
     save(path_for_result, encryptedImage)
 end
 
+
+"""
+    substitution_decryption(path_to_image, keys, path_for_result="./decrypted.png")
+
+Performs substitution decryption on a given image with the given keys.
+
+# Algorithm
+Iterates simulataneously over each pixel and key, and XORs the pixel value
+(all R, G, and B) with the given key. Hence, the keys provided must be the same
+as the ones provided during encryption.
+
+# Arguments
+- `path_to_image::String`: The path to the image to be decrypted.
+- `keys::Array{Int64, 1}`: Keys for decryption.
+- `path_for_result::String`: The path for storing the decrypted image.
+
+# Example
+```julia-repl
+julia> substitution_decryption("./encrypted.png", keys)
+DECRYPTING
+DECRYPTED
+```
+"""
 function substitution_decryption(
     path_to_image::String,
-    keys::Array,
+    keys::Array{Int64, 1},
     path_for_result::String="./decrypted.png",
 )
     image = load(path_to_image)
