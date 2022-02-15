69374406a2386c1b202c7ac7ceaf256394cc16a8
Saransh <saransh0701@gmail.com>
2022-02-15 18:04:13 +0530

    Imports

--- src/ChaoticEncryption.jl
+++ src/ChaoticEncryption.jl
@@ -2,5 +2,9 @@ module ChaoticEncryption
 
 include("lorenz_key.jl")
 include("logistic_key.jl")
+include("substitution.jl")
+
+export lorenz_key, logistic_key
+export substitution_encryption, substitution_decryption
 
 end
