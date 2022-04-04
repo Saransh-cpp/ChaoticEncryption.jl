befb3d0218c4265f4b592fb4730e8aeb56b2ae2f
Saransh <saransh0701@gmail.com>
2022-04-04 17:20:25 +0530

    Working!

--- src/substitution.jl
+++ src/substitution.jl
@@ -83,7 +83,7 @@ Iterates simulataneously over each pixel and key, and XORs the pixel value
 
 # Example
 ```jldoctest
-julia> using Image
+julia> using Images
 
 julia> img = load("../test_images/camera.jfif")
 225×225 Array{RGB{N0f8},2} with eltype RGB{N0f8}:
