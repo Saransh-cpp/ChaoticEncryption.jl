f8486bf1ece65af59ba040fb88c47ce1e32bf66c
Saransh <saransh0701@gmail.com>
2022-04-17 13:21:06 +0530

    Add module docstring (#68)

--- src/ChaoticEncryption.jl
+++ src/ChaoticEncryption.jl
@@ -1,3 +1,11 @@
+"""
+`ChaoticEncryption.jl` comes loaded with Pseudo-Random Number Generators and various
+encryption techniques, which can be used to encrypt and decrypt any image file. The
+package is under active development, but the existing API is stable and might not
+change significantly.
+
+For complete documentation, refer - https://saransh-cpp.github.io/ChaoticEncryption.jl
+"""
 module ChaoticEncryption
 
 include("lorenz_key.jl")
